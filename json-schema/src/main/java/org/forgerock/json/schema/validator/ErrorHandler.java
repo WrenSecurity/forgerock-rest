992b3438bf857cae86c85b7993ac67def4d8d19d
Kortanul <kortanul@protonmail.com>
2017-07-04 14:12:25 -0400

    fix documentation typos that block Javadoc gen

--- json-schema/src/main/java/org/forgerock/json/schema/validator/ErrorHandler.java
+++ json-schema/src/main/java/org/forgerock/json/schema/validator/ErrorHandler.java
@@ -2,6 +2,7 @@
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  * 
  * Copyright © 2011 ForgeRock AS. All rights reserved.
+ * Portions Copyright © 2017 Wren Security.
  * 
  * The contents of this file are subject to the terms
  * of the Common Development and Distribution License
@@ -29,11 +30,11 @@ import org.forgerock.json.schema.validator.exceptions.ValidationException;
 
 /**
  * ErrorHandler is the abstract base class for Validators.
- * <p>
- * If a Validator application needs to implementation of customized error
+ *
+ * <p>If a Validator application needs to implementation of customized error
  * handling, it must implement this class.
- * <p/>
- * Use this handler when call the
+ *
+ * <p>Use this handler when call the
  * {@link org.forgerock.json.schema.validator.validators.SimpleValidator#validate(Object, org.forgerock.json.fluent.JsonPointer, ErrorHandler)}}
  * method.  The helpers will then report all errors.</p>
  *
@@ -46,8 +47,8 @@ public abstract class ErrorHandler {
 
     /**
      * Process the <code>exception</code> of the Validator.
-     * <p/>
-     * None of the implementations of {Validator#validate} method throws exception. They call
+     *
+     * <p>None of the implementations of {Validator#validate} method throws exception. They call
      * this method to decide what to do if an exception occurs.
      *
      * @param exception the exception that the validator wants to handled
@@ -60,17 +61,17 @@ public abstract class ErrorHandler {
 
     /**
      * Receive notification of an error.
-     * <p/>
+     *
      * <p>For example, a validator would use this callback to
      * report the violation of a validity constraint.
      * The default behaviour is to take no action.</p>
-     * <p/>
+     *
      * <p>The validator must continue to provide normal validation
      * after invoking this method: it should still be possible
      * for the application to process the document through to the end.
      * If the application cannot do so, then the parser should report
      * a fatal error.</p>
-     * <p/>
+     *
      * <p>Filters may use this method to report other, non-JSON errors
      * as well.</p>
      *
@@ -84,9 +85,9 @@ public abstract class ErrorHandler {
 
     /**
      * Get the final result of the validation.
-     * <p/>
-     * The default value is <code>false</code>. If the validator has called the {#handleError} method
-     * then it return <code>true</code>.
+     *
+     * <p>The default value is <code>false</code>. If the validator has called the {#handleError}
+     * method then it return <code>true</code>.
      *
      * @return true if there was an error during the validation process.
      */
@@ -107,8 +108,8 @@ public abstract class ErrorHandler {
 
     /**
      * Throws an assembled exception after the validator finished the processing.
-     * <p/>
-     * Implementation of this method MUST throw an Exception if the {#error()} method
+     *
+     * <p>Implementation of this method MUST throw an Exception if the {#error()} method
      * was called on this instance before.
      *
      * @throws ValidationException when this instance wraps an error message(s).
